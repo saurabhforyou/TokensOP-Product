f7058d622152bc1c57a88f61416296a0bed0cd0c
saurabhforyou <101236666+saurabhforyou@users.noreply.github.com>
2022-03-09 10:44:51 +0530

    Product of two numbers

--- main.cpp
+++ main.cpp
@@ -2,12 +2,12 @@
 
 using namespace std;
 
-    int main()
+int main()
 {
-    cout << "Size of char: " << sizeof(char) << " byte" << endl;
-    cout << "Size of int: " << sizeof(int) << " bytes" << endl;
-    cout << "Size of float: " << sizeof(float) << " bytes" << endl;
-    cout << "Size of double: " << sizeof(double) << " bytes" << endl;
-
+    float a,b,product;
+    cout<<"Enter two numbers\n";
+    cin>>a>>b;
+    product=a*b;
+    cout<<"Product ="<<product;
     return 0;
 }
